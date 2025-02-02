d2052770e83f6e3f1f35bb480459e1714b201ac3
Avneet Kaur <avneetkaur@dyn220236.cc.lehigh.edu>
2025-02-02 18:32:06 -0500

    avneet worked on the homepage text

--- web/src/app/components/homepage_text.tsx
+++ web/src/app/components/homepage_text.tsx
@@ -7,7 +7,7 @@ export default function HomePageText() {
         <h3 id="slogan" className="text-white pt-8 leading-tight font-[Poppins] text-[86px] font-bold">
             Inspiring Innovators
           </h3>
-          <p id="blurb" className="text-left mt-9 text-white font-[Poppins] text-[18px]">
+          <p id="blurb" className="text-left w-[60vw] mt-9 text-white font-[Poppins] text-[18px]">
             Welcome to the Lehigh Entrepreneurship Club, the premier student-led organization dedicated to fostering entrepreneurial spirit and innovation on campus. We empower students to turn their ideas into reality through resources, events, and a strong network of aspiring founders and industry leaders.
           </p>
         <button id="joinNowButton" className=" bg-[#FBB040] mt-20 w-44  text-black font-[Poppins] text-2xl py-3 px-6 rounded-2xl border-2 border-black shadow-md hover:bg-[#f7a800] focus:outline-none focus:ring-2 focus:ring-[#FBB040] transition duration-300">
